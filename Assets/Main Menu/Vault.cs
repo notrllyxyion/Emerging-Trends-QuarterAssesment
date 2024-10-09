ac10774f4ccd732d49ac6742e514b4cb5a9cf2de
CIITSHSDTP02 <CIITSHSDTP02@ciit.edu.ph>
2024-10-09 08:59:12 +0800

    revert

--- Assets/Main Menu/Vault.cs
+++ Assets/Main Menu/Vault.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Vault : MonoBehaviour
 {
-    public void GoToVaultMenu()
+    // Start is called before the first frame update
+    void Start()
     {
-        SceneManager.LoadScene("Menu_Vault");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
